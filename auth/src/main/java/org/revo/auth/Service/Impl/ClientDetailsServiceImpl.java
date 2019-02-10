bb544992ab83745d84441625108cd569a650df22
ashraf <ashraf1abdelrasool@gmail.com>
2019-02-10 23:34:21 +0200

    push

--- auth/src/main/java/org/revo/auth/Service/Impl/ClientDetailsServiceImpl.java
+++ auth/src/main/java/org/revo/auth/Service/Impl/ClientDetailsServiceImpl.java
@@ -2,7 +2,6 @@ package org.revo.auth.Service.Impl;
 
 import org.revo.core.base.Config.Env;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
@@ -20,15 +19,13 @@ public class ClientDetailsServiceImpl implements ClientDetailsService {
     private PasswordEncoder encoder;
     @Autowired
     private Env env;
-    @Autowired
-    private DiscoveryClient discoveryClient;
 
     @Override
     public ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
         BaseClientDetails result = new BaseClientDetails();
         result.setClientId(clientId);
         result.setAuthorizedGrantTypes(Collections.singletonList("authorization_code"));
-        result.setRegisteredRedirectUri(new HashSet<>(Collections.singletonList(discoveryClient.getInstances("gateway").get(0).getUri() + "/login/oauth2/code/login-client")));
+        result.setRegisteredRedirectUri(new HashSet<>(Collections.singletonList(env.getServices().get("gateway").getUrl() + "/login/oauth2/code/login-client")));
         result.setClientSecret(encoder.encode(clientId));
         result.setScope(Arrays.asList("read", "write"));
         result.setAutoApproveScopes(Collections.singletonList("read"));
