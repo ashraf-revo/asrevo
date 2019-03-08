a804001a8089ebea626c674f3e689803c4f6bde8
ashraf <ashraf1abdelrasool@gmail.com>
2019-03-08 22:52:30 +0200

    push

--- core/base/src/main/java/org/revo/core/base/Domain/Ids.java
+++ core/base/src/main/java/org/revo/core/base/Domain/Ids.java
@@ -3,12 +3,14 @@ package org.revo.core.base.Domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 public class Ids {
     public Ids() {
+        this.ids = new ArrayList<>();
     }
 
     public Ids(List<String> ids) {
--- tube/src/main/java/org/revo/tube/Controller/MainController.java
+++ tube/src/main/java/org/revo/tube/Controller/MainController.java
@@ -10,7 +10,6 @@ import org.revo.tube.Config.Processor;
 import org.revo.tube.Service.FileService;
 import org.revo.tube.Service.IndexService;
 import org.revo.tube.Service.MasterService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.InputStreamResource;
@@ -24,62 +23,38 @@ import java.util.Collections;
 import java.util.Optional;
 
 import static java.lang.Integer.parseInt;
-import static org.springframework.web.reactive.function.server.RequestPredicates.*;
-import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Slf4j
 @Configuration
 public class MainController {
-    @Autowired
-    public FileService fileService;
-    @Autowired
-    public Processor processor;
-    @Autowired
-    private MasterService masterService;
-    @Autowired
-    private IndexService indexService;
-
     private final String masterURL = "/{master}.m3u8";
     private final String indexUrl = masterURL + "/{index}.m3u8";
     private final String keyUrl = masterURL + "/{master_id}.key";
 
-
     @Bean
-    public RouterFunction<ServerResponse> function() {
-        return nest(path("/api/file"), route(POST("/save"), serverRequest -> ok().body(serverRequest.bodyToMono(File.class)
-                        .map(it -> {
-                            it.setIp(serverRequest.exchange().getRequest().getHeaders().get("X-FORWARDED-FOR").get(0));
-                            return it;
-                        })
-                        .flatMap(it -> fileService.save(it))
-                        .doOnNext(it -> processor.file_queue().send(MessageBuilder.withPayload(it).build())).then()
-                , Void.class)))
-                .andNest(path("/api/master"),
-                        route(GET("/user/{id}"), serverRequest -> ok().body(masterService.findAll(Status.SUCCESS, 1000, null, Optional.of(serverRequest.pathVariable("id")).map(it -> {
-                            Ids ids = new Ids();
-                            ids.setIds(Collections.singletonList(it));
-                            return ids;
-                        }).orElse(new Ids()), new Ids()), Master.class))
-                                .andRoute(GET("/one/{id}"), serverRequest -> ok().body(masterService.findOne(serverRequest.pathVariable("id")), Master.class))
-                                .andRoute(GET(masterURL), serverRequest -> ok()
-                                        .header("Content-Type", "application/x-mpegURL")
-                                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + serverRequest.pathVariable("master") + ".m3u8")
-                                        .body(masterService.getStream(serverRequest.pathVariable("master")).map(its -> IOUtils.toInputStream(its, Charset.defaultCharset())).map(InputStreamResource::new), InputStreamResource.class))
-                                .andRoute(GET(indexUrl), serverRequest -> {
-                                    return ok()
-                                            .header("Content-Type", "application/x-mpegURL")
-                                            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + serverRequest.pathVariable("master") + ".m3u8")
-                                            .body(indexService.findOneParsed(serverRequest.pathVariable("master"), serverRequest.pathVariable("index")).map(its -> IOUtils.toInputStream(its, Charset.defaultCharset())).map(InputStreamResource::new), InputStreamResource.class);
-                                })
-                                .andRoute(GET(keyUrl), serverRequest -> ok()
-                                        .header("Content-Type", "application/pgp-keys")
-                                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + serverRequest.pathVariable("master") + ".key")
-                                        .body(masterService.findOne(serverRequest.pathVariable("master")).map(Master::getSecret).map(its -> IOUtils.toInputStream(its, Charset.defaultCharset())).map(InputStreamResource::new), InputStreamResource.class))
-                                .andRoute(GET("/{size}/{id}"), serverRequest -> ok().body(masterService.findAll(Status.SUCCESS, parseInt(serverRequest.pathVariable("size")), serverRequest.pathVariable("id").equals("0") ? null : serverRequest.pathVariable("id"), new Ids(), new Ids()), Master.class))
-                                .andRoute(POST("/{size}/{id}"), serverRequest -> ok().body(serverRequest.bodyToMono(Ids.class).flatMapMany(it -> masterService.findAll(Status.SUCCESS, parseInt(serverRequest.pathVariable("size")), serverRequest.pathVariable("id"), it, new Ids())), Master.class))
-                                .andRoute(POST("/"), serverRequest -> ok().body(serverRequest.bodyToMono(Ids.class).flatMapMany(it -> masterService.findAll(Status.SUCCESS, 1000, null, new Ids(), it)), Master.class)));
+    public RouterFunction<ServerResponse> function(FileService fileService, Processor processor, MasterService masterService, IndexService indexService) {
+        return route(POST("/api/file/save"), serverRequest -> ok().body(serverRequest.bodyToMono(File.class).flatMap(fileService::save).doOnNext(it -> processor.file_queue().send(MessageBuilder.withPayload(it).build())).then(), Void.class))
+                .andRoute(GET("/api/master/user/{id}"), serverRequest -> ok().body(masterService.findAll(Status.SUCCESS, 1000, null, Optional.of(serverRequest.pathVariable("id")).map(it -> new Ids(Collections.singletonList(it))).orElse(new Ids()), new Ids()), Master.class))
+                .andRoute(GET("/api/master/one/{id}"), serverRequest -> ok().body(masterService.findOne(serverRequest.pathVariable("id")), Master.class))
+                .andRoute(GET("/api/master" + masterURL), serverRequest -> ok()
+                        .header("Content-Type", "application/x-mpegURL")
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + serverRequest.pathVariable("master") + ".m3u8")
+                        .body(masterService.getStream(serverRequest.pathVariable("master")).map(its -> IOUtils.toInputStream(its, Charset.defaultCharset())).map(InputStreamResource::new), InputStreamResource.class))
+                .andRoute(GET("/api/master" + indexUrl), serverRequest -> ok()
+                        .header("Content-Type", "application/x-mpegURL")
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + serverRequest.pathVariable("master") + ".m3u8")
+                        .body(indexService.findOneParsed(serverRequest.pathVariable("master"), serverRequest.pathVariable("index")).map(its -> IOUtils.toInputStream(its, Charset.defaultCharset())).map(InputStreamResource::new), InputStreamResource.class))
+                .andRoute(GET("/api/master" + keyUrl), serverRequest -> ok()
+                        .header("Content-Type", "application/pgp-keys")
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + serverRequest.pathVariable("master") + ".key")
+                        .body(masterService.findOne(serverRequest.pathVariable("master")).map(Master::getSecret).map(its -> IOUtils.toInputStream(its, Charset.defaultCharset())).map(InputStreamResource::new), InputStreamResource.class))
+                .andRoute(GET("/api/master/{size}/{id}"), serverRequest -> ok().body(masterService.findAll(Status.SUCCESS, parseInt(serverRequest.pathVariable("size")), serverRequest.pathVariable("id").equals("0") ? null : serverRequest.pathVariable("id"), new Ids(), new Ids()), Master.class))
+                .andRoute(POST("/api/master/{size}/{id}"), serverRequest -> ok().body(serverRequest.bodyToMono(Ids.class).flatMapMany(it -> masterService.findAll(Status.SUCCESS, parseInt(serverRequest.pathVariable("size")), serverRequest.pathVariable("id"), it, new Ids())), Master.class))
+                .andRoute(POST("/api/master/"), serverRequest -> ok().body(serverRequest.bodyToMono(Ids.class).flatMapMany(it -> masterService.findAll(Status.SUCCESS, 1000, null, new Ids(), it)), Master.class));
     }
 
 }
